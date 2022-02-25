3d60ae98f8798e043e7635be68f7379cff038aa3
staff <battleroy7@gmail.com>
2022-02-25 13:49:43 +0300

    add collision

--- code/level.py
+++ code/level.py
@@ -43,6 +43,9 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        # creating the floor
+        self.floor_surface = 
+
     def custom_draw(self, player):
 
         #  getting the offset
