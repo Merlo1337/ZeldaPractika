e7e6a8dc3e182ed8f47e60fbd3c236b6f01fc178
staff <battleroy7@gmail.com>
2022-02-26 18:22:26 +0300

    add player animation

--- code/level.py
+++ code/level.py
@@ -48,12 +48,13 @@ class Level:
                             surf = graphics['objects'][int(col)]
                             Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
 
-        self.player = Player((2000, 1500), [self.visible_sprites], self.obstacle_sprites)
+        self.player = Player((2000, 1420), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        debug(self.player.status)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
